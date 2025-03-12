13f2d00bbdd2ed791d4f50b6deaa8572be8608ff
Akshat-Shrivastava11 <ak7.shrivastava@gmail.com>
2025-03-12 12:34:52 -0500

    added plots for all modules as well as stacked

--- Noiseanalysis.py
+++ Noiseanalysis.py
@@ -1,6 +1,6 @@
 import uproot
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 listofmodules_root= [
     '320-ML-F3CX-TT-0014/Completely_Encapsulated_2025-03-06/chamber_test1/pedestal_run/run_20250306_150110',
@@ -26,7 +26,7 @@ listofmodules_root= [
     ]
 
 mods = ['320-ML-F3CX-TT-0014','320-ML-F3CX-TT-0015','320-ML-F3CX-TT-0016','320-ML-F3CX-TT-0017','320-ML-F3CX-TT-0019','320-ML-F3CX-TT-0020'
-,'320-ML-F3CX-TT-M21','320-ML-F3CX-TT-M22','320-ML-F3CX-TT-M23','320-ML-F3CX-TT-M24','320-ML-F3CX-TT-M25','320-ML-F3CX-TT-0026'
+,'320-ML-F3CX-TT-M0021','320-ML-F3CX-TT-M0022','320-ML-F3CX-TT-M0023','320-ML-F3CX-TT-M0024','320-ML-F3CX-TT-M0025','320-ML-F3CX-TT-0026'
 ,'320-ML-F3CX-TT-0027','320-ML-F3CX-TT-0028','320-ML-F3CX-TT-0030','320-ML-F3CX-TT-0029','320-ML-F3CX-TT-0031','320-ML-F3CX-TT-0032'
 ]
 
@@ -69,21 +69,71 @@ def noisetudy(filename): #this is a root file from GUI
 
     return adc_mean,adc_stdd
 
-plt.figure(figsize=(12, 6))
+#fig, axes = plt.subplots(1, 2, figsize=(12, 6)) 
+all_adc_means = []
+all_adc_stdd = [] 
+
+Mean_of_adc_mean = []
+Mean_of_adc_std = []
+
 for mod_file,mod in zip(listofmodules_root,mods):
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))  
     fname = '/home/daq3/hgcal/data/'+mod_file+'/pedestal_run0.root'
     adc_mean,adc_stdd= noisetudy(fname)
-    plt.hist(adc_mean,bins = 50,histtype = 'step',label=mod)
-    plt.xlabel('adc_mean ')
-    plt.ylabel('Entries')
-    plt.title('ADC Mean')
-    #plt.xticks(rotation=90, fontsize=8)  
-    plt.legend()
 
-output_pdf = "adc_mean.pdf"
-plt.savefig(output_pdf, format="pdf", bbox_inches="tight")
-print(adc_mean)
-print(adc_stdd)
+    mean_val = np.mean(adc_mean)
+    std_val = np.std(adc_mean)
+
+    std_mean = np.mean(adc_stdd)
+    std_std = np.std(adc_stdd)
+    axes[0].hist(adc_mean, bins=70,histtype = 'step', label=f"{mod}\nMean: {mean_val:.2f}\nStd: {std_val:.2f}")
+    axes[0].set_xlabel('adc_mean')
+    axes[0].set_ylabel('Entries')
+    axes[0].set_title('ADC Mean ')
+    axes[0].set_xlim(0, 200)
+    axes[0].legend()
+
+    axes[1].hist(adc_stdd, bins=70, histtype = 'step',label=f"{mod}\nMean: {std_mean:.2f}\nStd: {std_std:.2f}")
+    axes[1].set_xlabel('adc_std(noise)')
+    axes[1].set_ylabel('Entries')
+    axes[1].set_title('ADC STD')
+    axes[1].set_xlim(0, 200)
+    axes[1].legend(loc = 'upper right')
+    all_adc_means.append(adc_mean)
+    all_adc_stdd.append(adc_stdd)
+    Mean_of_adc_mean.append([mean_val])
+    Mean_of_adc_std.append([std_mean])
+
+    output_jpg = f"outputplots/ADC_for_mod_{mod}.jpg"
+    plt.savefig(output_jpg, format="jpg", bbox_inches="tight")
+    #print(adc_mean)
+    #print(adc_stdd)
+    print(len(Mean_of_adc_mean))
+
+
+fig, axes = plt.subplots(1, 2, figsize=(20, 10))  
+axes[0].hist(Mean_of_adc_mean, bins = 18, histtype = 'step',stacked=True, label=mods)
+axes[0].set_xlabel('mean val adc_mean')
+axes[0].set_ylabel('Entries')
+axes[0].set_title('ADC Mean Means all mods')
+#axes[0].set_xlim(0, 250)
+
+
+
+axes[1].hist(Mean_of_adc_std,bins = 18,histtype = 'step',stacked=True, label=mods)
+axes[1].set_xlabel('adc_mean')
+axes[1].set_ylabel('Entries')
+axes[1].set_title('ADC mean for adc std all mods')
+axes[1].legend(loc = 'upper right')
+#axes[1].legend()
+#axes[1].set_xlim(0, 250)
+output_jpg = f"outputplots/ADC_all_mods.jpg"
+plt.savefig(output_jpg, format="jpg", bbox_inches="tight")
+
+#axes[1].legend()
+
+
+
 
 
 
