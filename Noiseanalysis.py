4ae38d30894c85c0e076c076a2c1e62401022c08
Akshat-Shrivastava11 <ak7.shrivastava@gmail.com>
2025-03-14 16:35:56 -0500

    got rid of stacked hists

--- Noiseanalysis.py
+++ Noiseanalysis.py
@@ -1,7 +1,8 @@
 import uproot
 import matplotlib.pyplot as plt
 import numpy as np
-
+#import mplhep as hep
+from matplotlib.ticker import MultipleLocator
 listofmodules_root= [
     '320-ML-F3CX-TT-0014/Completely_Encapsulated_2025-03-06/chamber_test1/pedestal_run/run_20250306_150110',
     '320-ML-F3CX-TT-0015/Completely_Encapsulated_2025-03-06/chamber_test1/pedestal_run/run_20250306_122700',
@@ -92,44 +93,56 @@ for mod_file,mod in zip(listofmodules_root,mods):
     axes[0].set_title('ADC Mean ')
     axes[0].set_xlim(0, 200)
     axes[0].legend()
-
+    # axes[0].yaxis.set_major_locator(MultipleLocator(0.1))
+    # axes[0].yaxis.set_minor_locator(MultipleLocator(0.05))
+    
     axes[1].hist(adc_stdd, bins=70, histtype = 'step',label=f"{mod}\nMean: {std_mean:.2f}\nStd: {std_std:.2f}")
     axes[1].set_xlabel('adc_std(noise)')
     axes[1].set_ylabel('Entries')
     axes[1].set_title('ADC STD')
     axes[1].set_xlim(0, 200)
-    axes[1].legend(loc = 'upper right')
+    
+    # axes[1].yaxis.set_major_locator(MultipleLocator(0.1))
+    # axes[1].yaxis.set_minor_locator(MultipleLocator(0.05))
     all_adc_means.append(adc_mean)
     all_adc_stdd.append(adc_stdd)
-    Mean_of_adc_mean.append([mean_val])
-    Mean_of_adc_std.append([std_mean])
-
-    output_jpg = f"outputplots/ADC_for_mod_{mod}.jpg"
+    #Mean_of_adc_mean.append([mean_val])
+    #Mean_of_adc_std.append([std_mean])
+    Mean_of_adc_mean.append(mean_val)
+    Mean_of_adc_std.append(std_mean)
+    output_jpg = f"outputplots/V3a/ADC_for_mod_{mod}.jpg"
     plt.savefig(output_jpg, format="jpg", bbox_inches="tight")
-    #print(adc_mean)
-    #print(adc_stdd)
+    plt.close(fig)
+    print(adc_mean)
+    print(adc_stdd)
     print(len(Mean_of_adc_mean))
 
 
 fig, axes = plt.subplots(1, 2, figsize=(20, 10))  
-axes[0].hist(Mean_of_adc_mean, bins = 18, histtype = 'step',stacked=True, label=mods)
-axes[0].set_xlabel('mean value adc_count_mean')
+#axes[0].hist(Mean_of_adc_mean, bins = 18 ,stacked=True, label=mods)
+axes[0].hist(Mean_of_adc_mean,histtype = 'step')
+axes[0].set_xlabel('adc__mean')
 axes[0].set_ylabel('Entries')
-axes[0].set_title('Mean of adc_mean values for all V3As @ TTU ')
+axes[0].set_title('Mean of adc_mean values for all V3As @ TTU Untrimmed')
+# axes[0].yaxis.set_major_locator(MultipleLocator(0.1))
+# axes[0].yaxis.set_minor_locator(MultipleLocator(0.05))
 #axes[0].set_xlim(0, 250)
 
 
 
-axes[1].hist(Mean_of_adc_std,bins = 18,histtype = 'step',stacked=True, label=mods)
-axes[1].set_xlabel('adc_mean')
+#axes[1].hist(Mean_of_adc_std,bins = 18,stacked=True, label=mods)
+axes[1].hist(Mean_of_adc_std,histtype = 'step')
+axes[1].set_xlabel('mean value Noise(adc_std)')
 axes[1].set_ylabel('Entries')
-axes[1].set_title('Mean of adc_std values for all V3As @ TTU ')
+axes[1].set_title('Mean of Noise(adc_std) values for all V3As @ TTU Untrimmed')
 axes[1].legend(loc = 'upper right')
+# axes[1].yaxis.set_major_locator(MultipleLocator(0.1))
+# axes[1].yaxis.set_minor_locator(MultipleLocator(0.05))
 #axes[1].legend()
 #axes[1].set_xlim(0, 250)
-output_jpg = f"outputplots/ADC_all_mods.jpg"
+output_jpg = f"outputplots/V3a/SummaryADC_mods.jpg"
 plt.savefig(output_jpg, format="jpg", bbox_inches="tight")
-
+plt.close(fig)
 #axes[1].legend()
 
 
